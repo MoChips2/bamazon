a5c88270d0fa70c8ed3661a084639193c2dd0a2f
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2018-12-15 10:06:19 -0600

    finished customer, manager apps

--- bamazonManager.js
+++ bamazonManager.js
@@ -6,13 +6,12 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Vikings-Win",
+    password: "",
     database: "bamazon"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    // insert starting function below
     managementTasks()
 });
 
